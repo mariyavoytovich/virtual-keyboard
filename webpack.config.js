6373e8d95e8e1b6295a0492c27ee2239d89447d8
mariyavoytovich <voytov.mariya@gmail.com>
2023-04-26 13:33:28 +0300

    feat: add sass and bundle config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
+const webpack = require('webpack');
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    watch: !isProduction,
+    entry: ['./src/js/index.js', './src/sass/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+          ]
+        }
+      ]
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })
+    ]
+  }
+
+  return config;
+}
